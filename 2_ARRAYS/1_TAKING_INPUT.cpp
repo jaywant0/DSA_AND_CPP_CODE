40368a774eca465b3ebc011e700f7e7fd0a18c5f
jaywant0 <jaywantmuktapure01.gmail.com>
2024-12-20 14:29:05 +0530

    new

--- 2_ARRAYS/1_TAKING_INPUT.cpp
+++ 2_ARRAYS/1_TAKING_INPUT.cpp
@@ -11,16 +11,26 @@ int main()
     //     cin>>arr[i];
     // }
     
-    char arr[26];
-   int n =26;
+//     char arr[26];
+//    int n =26;
 
-    for(char i = 0; i<n; i++){
-        cout<<"enter the chacter "<<endl;
-        cin>>arr[i];
-    }
+//     for(char i = 0; i<n; i++){
+//         cout<<"enter the chacter "<<endl;
+//         cin>>arr[i];
+//     }
 
-    for(char i = 0; i<5; i++){
-        cout<<arr[i]<<" ";
+//     for(char i = 0; i<5; i++){
+//         cout<<arr[i]<<" ";
+//     }
+
+    // jay can do best code ever 
+
+    int a;
+    cout<<"Enter the number"<<endl;
+    
+    cin>>a;
+    for(int i = 0; i< a; i++){
+        cout<<"hello world"<<endl;
     }
 
     return 0;
